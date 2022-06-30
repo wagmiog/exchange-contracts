976d79ec51176356a0059dad837f05840faf8420
ClementMoyal <clementmoyal@gmail.com>
2022-06-30 08:36:11 +0200

    Vampire script

---
+++ Near/scripts/Nodejs/testnet/VampireSwap/set_delay.js
@@ -0,0 +1,67 @@
+const HELP = `Please run this script in the following format:
+    node extend_operators.js owner lock_delay
+`;
+const { connect, KeyPair, keyStores, utils, WalletConnection, Contract } = require("near-api-js");
+const fs = require("fs")
+const path = require("path");
+const homedir = require("os").homedir();
+const { CONTRACTS, OWNER, MINTER, FEES } = require("../../nearConfig");
+const { exchangeConst, farmingConst, tokenConst, xTokenConst } = require("../../constants-testnet");
+const { functionCall } = require("near-api-js/lib/transaction");
+const CREDENTIALS_DIR = ".near-credentials";
+const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
+const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+
+if (process.argv.length !== 4) {
+    console.info(HELP);
+    process.exit(1);
+  }
+
+const config = {
+  keyStore,
+  networkId: "testnet",
+  nodeUrl: "https://rpc.testnet.near.org",
+};
+
+main(process.argv[2]);
+
+async function main(deployerAccount) {
+    const near = await connect({ ...config, keyStore });
+    const deployer = await near.account(deployerAccount);
+    await checkIfFullAccess([ deployer ]);
+    const FARMING = await initializeFarming(deployer, CONTRACTS.vampire);
+    await FARMING.modify_lock_delay({
+        args: {
+            lock_delay: parseInt(process.argv[3]),
+        },
+        amount: "1",
+    });
+    console.log("New lock delay:", await FARMING.get_lock_delay());
+}
+
+async function checkIfFullAccess(contractsAccounts) {
+    for (let i = 0; i < contractsAccounts.length; i++) {
+      let contractAccount = await contractsAccounts[i].findAccessKey();
+      if (await contractAccount === null) {
+        console.log("⚠️", await contractsAccounts[i].accountId, "need access Key ⚠️");
+        process.exit(1);
+      } else if (contractAccount.accessKey.permission !== "FullAccess") {
+        console.log("⚠️", await contractsAccounts[i].accountId, "need FULL access Key ⚠️");
+        process.exit(1);
+      }
+    }
+    console.log("Full Access Key ✅");
+}
+
+async function initializeFarming(deployer, farming) {
+  const FARMING = new Contract(
+      deployer,
+      farming,
+      {
+        viewMethods: [ "get_lock_delay" ],
+        changeMethods: [ "modify_lock_delay" ],
+        sender: deployer
+      }
+  );
+  return FARMING;
+}
